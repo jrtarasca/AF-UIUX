699d2fda716baea70e2a33d7ffd3a703908126ca
jrtarasca <jrtarasca294@gmail.com>
2025-11-26 20:55:24 -0300

    modified:   calculadora/Calculadora.java 	modified:   calculadora/TesteCalculadora.java

--- calculadora/TesteCalculadora.java
+++ calculadora/TesteCalculadora.java
@@ -1,19 +1,23 @@
-
 public class TesteCalculadora {
+
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        // Testes de operações válidas
-        calc.calc(2, 3, "+"); // Esperado: 5
-        calc.calc(10, 4, "-"); // Esperado: 6
-        calc.calc(3, 5, "*"); // Esperado: 15
-        calc.calc(8, 2, "/"); // Esperado: 4
-        calc.calc(8, 0, "/"); // divisão por zero
-        calc.calc(5, 5, "x"); // operação inválida
+        System.out.println(calc.calcular( 2,  3, "+"));
+        System.out.println(calc.calcular( 10,  4, "-"));
+        System.out.println(calc.calcular( 3,  5, "*"));
+        System.out.println(calc.calcular( 8,  2, "/"));
 
-        // Teste de operação inválida
-        calc.calc(10, 5, "%"); // Esperado: Operacao invalida
+        try {
+            System.out.println(calc.calcular( 8,  0, "/")); // exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(calc.calcular( 5,  5,  "x")); // exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
-
-    
